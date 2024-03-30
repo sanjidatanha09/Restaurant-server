46ac53ceb419529aca189fee9ba11ca184a0928a
sanjidatanha09 <sanjidatanha09@gmail.com>
2024-03-30 10:17:12 +0600

    get data create

--- index.js
+++ index.js
@@ -28,12 +28,30 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
+
+        const menuCollection = client.db("FancyRestuarant").collection("menu");
+
+        app.get('/getMenuData', async(req,res) =>{
+            const result = await menuCollection.find().toArray();
+            res.send(result)
+        })
+       
+
+
+
+
+
+
+
+
+
+
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
